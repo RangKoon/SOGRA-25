929060641fa0a1c92342d7271ac91d391c9167fb
RangKoon <wag00001@o.cnu.ac.kr>
2025-11-09 12:54:48 +0900

    하드코딩 수정

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 // Firebase 설정
 // TODO: Firebase 콘솔에서 프로젝트를 생성하고 아래 설정을 업데이트하세요
 const firebaseConfig = {
-  apiKey: "AIzaSyDVytMRnNPYsnGSbiJeDM3wPclfiODgVjo",
-  authDomain: "sogra-25.firebaseapp.com",
-  projectId: "sogra-25",
-  storageBucket: "sogra-25.firebasestorage.app",
-  messagingSenderId: "123738915963",
-  appId: "1:123738915963:web:a5c2182cb4f9fe0cf20ede",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Firebase 초기화
